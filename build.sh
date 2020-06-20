f0db8e47a198a32e7d3572515d11fb87d8b89b01
Paco Pascal <me@pacopascal.com>
2020-06-20 18:30:27 +0000

    Minor improvements

--- build.sh
+++ build.sh
@@ -24,17 +24,10 @@ set -e
 where_am_i() {
 	if [ -e "C:\\" ]; then
 		echo "WINDOWS"
-		return
-	fi
-	
-	if [ `uname` = "Linux" ]; then
+	elif [ `uname` = "Linux" ]; then
 		echo "LINUX"
-		return
-	fi
-
-	if [ `uname` = "Haiku" ]; then
+	elif [ `uname` = "Haiku" ]; then
 		echo "HAIKU"
-		return
 	fi
 }
 
@@ -86,6 +79,22 @@ compile_cpp_debug() {
 	clang-cpp -Wall -Werror -g -O0 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
 }
 
+compile_gcc_c_debug() {
+	gcc -Wall -Werror -g -O0 -std=c99 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2} 
+}
+
+compile_gcc_cpp_debug() {
+	g++ -Wall -Werror -g -O0 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
+}
+
+compile_gcc_c_release() {
+	gcc -Wall -Werror -O2 -std=c99 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
+}
+
+compile_gcc_cpp_release() {
+	g++ -Wall -Werror -O2 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
+}
+
 make_dicts() {
 	./tools/wl2array.py resources/en-common.wl common6 6 > src/dicts/common6.c
 	#./tools/wl2array.py resources/en-common.wl common > src/dicts/common.c
@@ -96,24 +105,8 @@ make_dicts() {
 build_haiku() {
 	cc=${1}
 	
-	compile_c_debug() {
-		gcc -Wall -Werror -g -O0 -std=c99 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2} 
-	}
-	
-	compile_cpp_debug() {
-		g++ -Wall -Werror -g -O0 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
-	}
-	
-	compile_c_release() {
-		gcc -Wall -Werror -O2 -std=c99 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
-	}
-	
-	compile_cpp_release() {
-		g++ -Wall -Werror -O2 -Isrc/common/ -Isrc/dicts/ -o ${1} -c ${2}
-	}
-	
-	compile_all compile_c_${cc} < build_common.txt
-	compile_all compile_cpp_${cc} < build_haiku.txt
+	compile_all compile_gcc_c_${cc} < build_common.txt
+	compile_all compile_gcc_cpp_${cc} < build_haiku.txt
 	g++ -o build/mm -lbe build/trie.o build/words.o build/game.o build/main.o build/common6.o
 }
 
@@ -131,7 +124,6 @@ build_win() {
 	compile_all compile_c_${cc} < build_common.txt
 	compile_all compile_c_${cc} < build_win.txt
 	clang -o build/mm.exe -l user32.lib -l gdi32.lib build/trie.o build/words.o build/game.o build/common6.o build/mm.o build/window_action.o build/window_input.o build/window_playboard.o
-	
 }
 
 # build [compile command]
@@ -150,6 +142,8 @@ build() {
 		'HAIKU')
 			build_haiku ${cc}
 		;;
+		*)
+			echo "OS not supported."
 	esac
 }
 
